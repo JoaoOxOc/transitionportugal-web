e1f48335093fd52655c13164cb32f8323817bcfe
JoaoOxOc <jp_69_7@hotmail.com>
2022-02-07 20:43:51 +0000

    reverting

--- frontends/packages/tp_backoffice/config.js
+++ frontends/packages/tp_backoffice/config.js
@@ -1,4 +1,4 @@
 export const auth0Config = {
-    client_id: 'HjCRLPMP4gtkS2fmzuPf7whataQL4ODD',//process.env.REACT_APP_AUTH0_CLIENT_ID,
-    domain: 'transportugal.eu.auth0.com'//process.env.REACT_APP_AUTH0_DOMAIN
+    client_id: process.env.REACT_APP_AUTH0_CLIENT_ID,
+    domain: process.env.REACT_APP_AUTH0_DOMAIN
 };
