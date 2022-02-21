12fd9a2db7c18c9effabc97480f3d8f07ec214af
JoaoOxOc <JoaoOxOc@users.noreply.github.com>
2022-02-21 18:45:48 +0000

    Update Program.cs

--- tp_webservices/APIs/UserService/Program.cs
+++ tp_webservices/APIs/UserService/Program.cs
@@ -90,6 +90,7 @@ try
 {
     IPHostEntry host = Dns.GetHostEntry("db-postgresql-lon1-49463-do-user-7785829-0.b.db.ondigitalocean.com");
     Console.WriteLine($"GetHostEntry(db-postgresql-lon1-49463-do-user-7785829-0.b.db.ondigitalocean.com) returns HostName: {host.HostName}");
+    throw new Exception($"GetHostEntry(db-postgresql-lon1-49463-do-user-7785829-0.b.db.ondigitalocean.com) returns HostName: {host.HostName}");
 }
 catch (Exception e)
 {
