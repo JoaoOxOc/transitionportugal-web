065c493cc2a5665a5e7fade499683d0a58902c96
JoaoOxOc <jp_69_7@hotmail.com>
2022-04-06 15:55:17 +0100

    updatedby and createdby should have string type

--- tp_webservices/APIs/UserService/Migrations/20220406145354_set_userid_string.Designer.cs
+++ tp_webservices/APIs/UserService/Migrations/20220406145354_set_userid_string.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using UserService.Services.Database;
@@ -11,9 +12,10 @@ using UserService.Services.Database;
 namespace UserService.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20220406145354_set_userid_string")]
+    partial class set_userid_string
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ tp_webservices/APIs/UserService/Migrations/20220406145354_set_userid_string.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserService.Migrations
+{
+    public partial class set_userid_string : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UpdatedBy",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedBy",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "179642d9-0f10-4d7d-a1a0-b485b3f6659f",
+                column: "ConcurrencyStamp",
+                value: "a8a8e58d-0b79-426e-bde0-2dacb70c4a3c");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "244d3f1a-8594-4adb-9c59-5ec36fcdbf03",
+                column: "ConcurrencyStamp",
+                value: "2d002376-92c1-44a5-947b-37467119870e");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "69d5274f-235d-4013-bbac-0c4eddf31ecc",
+                column: "ConcurrencyStamp",
+                value: "bb6ee11d-1657-4bbf-8cd1-4a92ef65393f");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e762fd61-0f58-4c5d-ad0e-7bd322ae3ccf",
+                column: "ConcurrencyStamp",
+                value: "a0c17a8c-7674-46e1-bcbd-57357a6b2be6");
+
+            migrationBuilder.UpdateData(
+                table: "Setting",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2022, 4, 6, 15, 53, 54, 254, DateTimeKind.Local).AddTicks(6936));
+
+            migrationBuilder.UpdateData(
+                table: "Setting",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2022, 4, 6, 15, 53, 54, 254, DateTimeKind.Local).AddTicks(6974));
+
+            migrationBuilder.UpdateData(
+                table: "Setting",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2022, 4, 6, 15, 53, 54, 254, DateTimeKind.Local).AddTicks(6977));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "UpdatedBy",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CreatedBy",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "179642d9-0f10-4d7d-a1a0-b485b3f6659f",
+                column: "ConcurrencyStamp",
+                value: "9a751937-518d-4dce-a712-c6091d69dfe3");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "244d3f1a-8594-4adb-9c59-5ec36fcdbf03",
+                column: "ConcurrencyStamp",
+                value: "f6837a6b-aa5f-4537-a80f-cebe1ebf105c");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "69d5274f-235d-4013-bbac-0c4eddf31ecc",
+                column: "ConcurrencyStamp",
+                value: "dacbfd44-634d-4af7-b796-f109173b1f63");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e762fd61-0f58-4c5d-ad0e-7bd322ae3ccf",
+                column: "ConcurrencyStamp",
+                value: "ec0f5635-fbe8-4621-aa21-f6703cbc0254");
+
+            migrationBuilder.UpdateData(
+                table: "Setting",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2022, 3, 12, 17, 35, 12, 122, DateTimeKind.Local).AddTicks(525));
+
+            migrationBuilder.UpdateData(
+                table: "Setting",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2022, 3, 12, 17, 35, 12, 122, DateTimeKind.Local).AddTicks(591));
+
+            migrationBuilder.UpdateData(
+                table: "Setting",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2022, 3, 12, 17, 35, 12, 122, DateTimeKind.Local).AddTicks(594));
+        }
+    }
+}
