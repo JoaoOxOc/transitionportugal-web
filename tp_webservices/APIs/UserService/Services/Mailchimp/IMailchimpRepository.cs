a1b566ba80862cacb16ba8324c7b7df9963c9c9c
JoaoOxOc <jp_69_7@hotmail.com>
2022-05-02 11:01:41 +0100

    added mailchimp repository

---
+++ tp_webservices/APIs/UserService/Services/Mailchimp/IMailchimpRepository.cs
@@ -0,0 +1,12 @@
+﻿using MailChimp.Net.Models;
+
+namespace UserService.Services.Mailchimp
+{
+    public interface IMailchimpRepository
+    {
+        List<Template> GetAllTemplates();
+        List<List> GetAllMailingLists();
+        Content GetTemplateDefaultContent(string templateId);
+        Content CreateAndSendCampaign(string html, int templateId, string listId, string replyToEmail, string fromName, string emailTitle, string emailSubject);
+    }
+}
