48e5587da80bf8b5d3b8c99414bbed9c048c652f
JoaoOxOc <jp_69_7@hotmail.com>
2022-05-18 16:04:56 +0100

    added new geolocation package

--- frontends/packages/tp_backoffice/next.config.js
+++ frontends/packages/tp_backoffice/next.config.js
@@ -1,7 +1,7 @@
 
 const optimizedImages = require('next-optimized-images');
 const withPlugins = require("next-compose-plugins");
-const withTM = require("next-transpile-modules")(["@transitionpt/translations"], { setExternals: true });
+const withTM = require("next-transpile-modules")(["@transitionpt/translations","@transitionpt/geolocation"], { setExternals: true });
 
 module.exports = withPlugins([optimizedImages, withTM], {
     reactStrictMode: true,
