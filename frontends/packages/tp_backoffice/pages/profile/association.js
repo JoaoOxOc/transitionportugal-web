29c37af1ad875781cc2e700781b9cc13ed98cad2
JoaoOxOc <jp_69_7@hotmail.com>
2022-05-27 13:36:31 +0100

    building association profile page

--- frontends/packages/tp_backoffice/pages/profile/association.js
+++ frontends/packages/tp_backoffice/pages/profile/association.js
@@ -1,27 +1,31 @@
-import { useState, useEffect } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 
 import Head from 'next/head';
 import {getSession} from "next-auth/react";
 
+import { useRefMounted } from '../../hooks/useRefMounted';
 import AccentHeaderLayout from '../../layouts/AccentHeaderLayout';
 import { Authenticated } from '../../components/Authenticated';
 import { Authorized } from '../../components/Authorized';
+import { GetAssociationData } from '../../services/associations';
 import { getUserIdAndAssociation } from '../../utils/jwt';
 
 import Footer from '../../components/Footer';
 
 import { i18nextAssociationDetails } from "@transitionpt/translations";
-import ProfileCover from '../../content/Profile/Association/ProfileCover';
-import AssociationDetails from '../../content/Management/Associations/single/details';
+import ProfileCover from '../../content/Management/Associations/single/ProfileCover';
 
 
-function AssociationProfileView() {
+function AssociationProfileView(props) {
+  console.log(props);
     const { t } = i18nextAssociationDetails;
     const [currentLang, setLang] = useState("pt");
     i18nextAssociationDetails.changeLanguage(currentLang);
 
-    const userData = getUserIdAndAssociation(window.localStorage.getItem('accessToken'));
-    console.log(userData);
+    // const userData = getUserIdAndAssociation(window.localStorage.getItem('accessToken'));
+    // console.log(userData);
+
+    // consultar: https://tokyo-black-nextjs-js.bloomui.com/management/users/single/1
 
     useEffect(() => {
         const handleNewMessage = (event) => {
@@ -37,6 +41,7 @@ function AssociationProfileView() {
         <title>{t('LABELS.associationDetails')}</title>
       </Head>
 
+      <ProfileCover association={props.associationData}/>
       {/* <AssociationDetails isCreate={false} isProfile={true} associationId={userData.associationId}/> */}
       <Footer />
     </>
@@ -48,7 +53,7 @@ AssociationProfileView.getLayout = (page) => {
   const { props } = page;
   return (
     <Authenticated session={props.children.props.session}>
-      <Authorized session={props.children.props.session} scopes={["users.write","association.admin"]}>
+      <Authorized session={props.children.props.session} scopes={["association.read","association.admin"]}>
         <AccentHeaderLayout>{page}</AccentHeaderLayout>
       </Authorized>
     </Authenticated>
@@ -56,11 +61,26 @@ AssociationProfileView.getLayout = (page) => {
 }
 
 export const getServerSideProps = async (context) => {
+  let associationUri = "/association/profile";
   // get the session
   const session = await getSession(context);
+  let associationGetResponse = null;
+  let associationGetResponseError = null;
+  try {
+    associationGetResponse = await GetAssociationData(process.env.NEXT_PUBLIC_API_BASE_URL + associationUri, session.accessToken);
+    console.log(associationGetResponse)
+    
+    if (associationGetResponse.status) {
+      associationGetResponseError = associationGetResponse;
+    }
+  } catch (err) {
+    associationGetResponseError = err;
+  }
+
+  const associationData = associationGetResponseError ? { session: session, associationProfileError: associationGetResponseError } : {session: session, associationData: associationGetResponse.associationData};
 
   // passing the session object to the page  
-  return { props: {session: session} };
+  return { props: associationData };
 };
 
 export default AssociationProfileView;
