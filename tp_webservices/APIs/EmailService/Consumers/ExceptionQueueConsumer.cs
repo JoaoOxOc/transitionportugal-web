0406c3f83b88185327066f38bb9103cb53b41b26
JoaoOxOc <jp_69_7@hotmail.com>
2022-06-10 16:59:44 +0100

    created exception queue consumer

---
+++ tp_webservices/APIs/EmailService/Consumers/ExceptionQueueConsumer.cs
@@ -0,0 +1,26 @@
+﻿using CommonLibrary.Entities.ViewModel;
+using EmailService.Code;
+using EmailService.Repositories;
+using EmailService.Sender;
+using MassTransit;
+
+namespace EmailService.Consumers
+{
+    public class ExceptionQueueConsumer : IExceptionQueueConsumer, IConsumer<CommonLibrary.Entities.ViewModel.ExceptionModel>
+    {
+        private readonly IEmailService _emailService;
+        private readonly IEmailTemplatesRepository _emailTemplateRepo;
+
+        public ExceptionQueueConsumer(IEmailService emailService, IEmailTemplatesRepository emailTemplateRepo)
+        {
+            _emailService = emailService;
+            _emailTemplateRepo = emailTemplateRepo;
+        }
+        public async Task Consume(ConsumeContext<ExceptionModel> context)
+        {
+            //var mailData = EmailTemplateReplacer.ProcessEmailTemplate(_emailTemplateRepo, context.Message);
+            //bool sent = _emailService.SendMail(mailData.To, mailData.Subject, mailData.Body);
+            // TODO: deal with message not sent
+        }
+    }
+}
