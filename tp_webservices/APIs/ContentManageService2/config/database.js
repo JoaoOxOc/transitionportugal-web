fa1ac1f631c3cfa1f5fce9d6d259ffad897dfb8b
JoaoOxOc <jp_69_7@hotmail.com>
2022-07-12 18:06:25 +0100

    added the banner collection type and the Seo and Meta components

--- tp_webservices/APIs/ContentManageService2/config/database.js
+++ tp_webservices/APIs/ContentManageService2/config/database.js
@@ -5,9 +5,9 @@
     connection: {
       host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'db'),
+      database: env('DATABASE_NAME', 'strapidb'),
       user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '0000'),
+      password: env('DATABASE_PASSWORD', 'postgres'),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
       ssl: env('DATABASE_SSL', false)
       // ssl: {
