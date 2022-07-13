200c57bf03d51c8da3dab32f8d937c291d5e1e31
JoaoOxOc <jp_69_7@hotmail.com>
2022-07-13 10:49:07 +0100

    enabling images sources in strapi

--- tp_webservices/APIs/ContentManageService2/config/middlewares.js
+++ tp_webservices/APIs/ContentManageService2/config/middlewares.js
@@ -1,6 +1,20 @@
+/**
+ * https://github.com/strapi/strapi/issues/11637
+ * https://stackoverflow.com/questions/40360109/content-security-policy-img-src-self-data
+ */
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          // 'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+          'img-src': ["'self'", 'data:', 'https:', 'cdn.jsdelivr.net', 'strapi.io'],
+        },
+      }
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
