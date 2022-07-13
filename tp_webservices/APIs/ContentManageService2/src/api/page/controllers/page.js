d383c321752aac58a4526d175b3d5de5d35d9bed
JoaoOxOc <jp_69_7@hotmail.com>
2022-07-13 10:50:34 +0100

    page blocks created - carousel now available

--- tp_webservices/APIs/ContentManageService2/src/api/page/controllers/page.js
+++ tp_webservices/APIs/ContentManageService2/src/api/page/controllers/page.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  banner controller
+ *  page controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::banner.banner');
+module.exports = createCoreController('api::page.page');
--- tp_webservices/APIs/ContentManageService2/src/api/page/services/page.js
+++ tp_webservices/APIs/ContentManageService2/src/api/page/services/page.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * banner service.
+ * page service.
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::banner.banner');
+module.exports = createCoreService('api::page.page');
