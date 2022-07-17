d2323967346bcc8b20f915626ece33298e95a63f
JoaoOxOc <jp_69_7@hotmail.com>
2022-07-17 18:28:31 +0100

    removed nookies

--- frontends/packages/tp_backoffice/layouts/AccentHeaderLayout/Header/Userbox/index.js
+++ frontends/packages/tp_backoffice/layouts/AccentHeaderLayout/Header/Userbox/index.js
@@ -22,7 +22,7 @@ import AccountTreeTwoToneIcon from '@mui/icons-material/AccountTreeTwoTone';
 import Link from '../../../../components/Link';
 import { i18nextSidemenu } from "@transitionpt/translations";
 import { useSession, signOut } from "next-auth/react";
-import { destroyClientCookies } from '../../../../services/cookies';
+// import { destroyClientCookies } from '../../../../services/cookies';
 
 const UserBoxButton = styled(Button)(
   ({ theme }) => `
