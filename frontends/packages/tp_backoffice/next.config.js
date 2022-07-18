88c38260a180d66a516c4b3420548e4741c18497
JoaoOxOc <JoaoOxOc@users.noreply.github.com>
2022-07-18 14:20:47 +0100

    Update next.config.js

--- frontends/packages/tp_backoffice/next.config.js
+++ frontends/packages/tp_backoffice/next.config.js
@@ -4,7 +4,7 @@ const withPlugins = require("next-compose-plugins");
 const withTM = require("next-transpile-modules")(["@transitionpt/translations", "@transitionpt/geolocation", "@transitionpt/components"], { setExternals: true });
 
 module.exports = withPlugins([optimizedImages, withTM], {
-    basePath: '/admin',
+    basePath: '/',
     reactStrictMode: true,
     images: {
       disableStaticImages: true
