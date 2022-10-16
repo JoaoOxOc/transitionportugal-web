bb8e57db85026f02c8000feda86daa7dddcfc161
JoaoOxOc <jp_69_7@hotmail.com>
2022-10-16 16:07:00 +0100

    TP_HOME: docker new config

--- tp_webservices/APIs/ContentManageService2/config/database.js
+++ tp_webservices/APIs/ContentManageService2/config/database.js
@@ -3,16 +3,13 @@
   connection: {
     client: env('DATABASE_CLIENT', 'postgres'),
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 5432),
+      host: env('DATABASE_HOST', 'host.docker.internal'),
+      port: env.int('DATABASE_PORT', 5433),
       database: env('DATABASE_NAME', 'strapidb'),
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', 'postgres'),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
-      ssl: env('DATABASE_SSL', false) == false ? false : {
-        require: env('DATABASE_SSL', false),
-        rejectUnauthorized: false
-      },
+      ssl: false,
     },
     debug: false,
   },
