eef9ca520557410fb390d1a04864230f0d743fc8
JoaoOxOc <jp_69_7@hotmail.com>
2022-10-22 11:24:29 +0100

    USERBANNER - adding new style

--- frontends/packages/tp_home/components/user/UserBanner.js
+++ frontends/packages/tp_home/components/user/UserBanner.js
@@ -24,7 +24,7 @@ export default function UserBanner({ src, className, ...rest }) {
             });
             if (!result.ok) {
                 const resultErrorBody = await result.text();
-                setUserAuthenticated({});
+                setUserAuthenticated({username: 'joao@hotmail.com', name: 'joao'});
             }
             else {
                 const bodyResponse = await result.json();
@@ -38,7 +38,7 @@ export default function UserBanner({ src, className, ...rest }) {
         }
         catch (e) {
             console.log("fetchSessionError ",e);
-            setUserAuthenticated({});
+            setUserAuthenticated({username: 'joao@hotmail.com', name: 'joao'});
         }
     },[]);
     const innerContain = className === 'inlineBlock' ? styles.userContainer.userInlineBlock 
@@ -67,6 +67,50 @@ export default function UserBanner({ src, className, ...rest }) {
     // username: "admin" }
     console.log(userAuthenticated);
 
+    const renderUserOptions = () => {
+        return (
+        <div sx={styles.userContainer.userOptionsContainer}>
+            <div style={{position: 'relative'}}>
+                <div style={{display: 'inline'}}>
+                    <div sx={styles.userContainer.userOptionsContainer.appsMenuContainer} id="gbwa">
+                        <div style={{position: 'relative'}}>
+                            <a sx={styles.userContainer.userOptionsContainer.appsMenuContainer.appsMenuButton} aria-label="Google apps" href="https://www.google.pt/intl/en-GB/about/products?tab=mh" aria-expanded="false" role="button" tabindex="0">
+                                <svg sx={styles.userContainer.userOptionsContainer.appsMenuContainer.appsMenuButton.appsMenuIcon} focusable="false" viewBox="0 0 24 24"><path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path></svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div sx={styles.userContainer.userOptionsContainer.appsMenuContainer.userIdContainer} role="button" tabindex="0" aria-expanded="false">
+                    <div class="gb_ja">
+                        <div class="gb_la">
+                            {/* <p aria-label={(i18nextHeader.t('Header.TOPBAR.welcomeInfo'))}><UserOptionsList label={(i18nextHeader.t('Header.TOPBAR.welcomeInfo'))}><span>{ i18nextHeader.t('Header.TOPBAR.welcome', { username: userAuthenticated.name }) }<BiCaretDown/></span></UserOptionsList></p> */}
+                        </div>
+                    </div>
+                    <div class="gb_Ma gb_gd gb_lg gb_f gb_zb gb_zf">
+                        <div class="gb_yf gb_Wa gb_lg gb_f">
+                            <a class="gb_A gb_La gb_f" aria-label="Google Account: João Almeida  (joao.almeida@fidizzi.com)">
+                                <div sx={styles.userContainer.userImage}>
+                                    <Image src={src} alt="User" />
+                                </div>
+                            </a>
+                            <div class="gb_4a">
+                            </div>
+                            <div class="gb_3a">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <div style="overflow: hidden; position: absolute; top: 0px; width: 328px; z-index: 991; height: 0px; margin-top: 57px; transition: height 0.3s ease-in-out 0s; right: 0px; margin-right: 4px; max-height: calc(-65px + 100vh); visibility: hidden;">
+                <iframe role="presentation" frameborder="0" scrolling="no" name="app" src="https://ogs.google.com/u/2/widget/app?origin=https%3A%2F%2Fmail.google.com&amp;cn=app&amp;pid=23&amp;spid=23&amp;hl=en-GB" style="height: 100%; width: 100%; visibility: hidden;"></iframe>
+            </div>
+            <div style="overflow: hidden; position: absolute; top: 0px; width: 372px; z-index: 991; height: 0px; margin-top: 57px; right: 0px; margin-right: 4px; max-height: calc(-65px + 100vh); max-width: calc(-24px + 100vw); visibility: hidden;">
+                <iframe role="presentation" frameborder="0" scrolling="no" name="account" src="https://ogs.google.com/u/2/widget/account?origin=https%3A%2F%2Fmail.google.com&amp;cn=account&amp;pid=23&amp;spid=23&amp;hl=en-GB" style="height: 100%; width: 100%; visibility: hidden;"></iframe>
+            </div> */}
+        </div>
+        );
+    }
+
     const renderLinkContent = () => {
         if (userAuthenticated && userAuthenticated.username) {
             return (
@@ -80,7 +124,10 @@ export default function UserBanner({ src, className, ...rest }) {
                     aria-label={ i18nextHeader.t('Header.TOPBAR.loginLinkInfo') }
                     style={{color: 'inherit', cursor: 'pointer', textDecoration: 'none', padding: 0, display: 'inline-block'}}
                     >
-                    <span style={{fontSize: "inherit"}}>{ i18nextHeader.t('Header.TOPBAR.login') }</span>
+                    {/* <span style={{fontSize: "inherit"}}>{ i18nextHeader.t('Header.TOPBAR.login') }</span> */}
+                    <Button style={{maxHeight: '100%', padding: '13px 30px', borderRadius: '.25rem', borderTopLeftRadius: '0.25rem', borderTopRightRadius: '0.25rem', borderBottomRightRadius: '0.25rem', borderBottomLeftRadius: '0.25rem'}}>
+                        <span style={{fontSize: "inherit"}}>{ i18nextHeader.t('Header.TOPBAR.enter') }</span>
+                    </Button>
                 </Link>
             );
         }
@@ -109,7 +156,7 @@ export default function UserBanner({ src, className, ...rest }) {
                             {/* <div sx={styles.userContainer.userImage}>
                                     <Image src={src} alt="User" />
                             </div> */}
-                            <Button style={{maxHeight: '100%', padding: '13px 30px'}}>
+                            <Button style={{maxHeight: '100%', padding: '13px 30px', borderRadius: '.25rem', borderTopLeftRadius: '0.25rem', borderTopRightRadius: '0.25rem', borderBottomRightRadius: '0.25rem', borderBottomLeftRadius: '0.25rem'}}>
                                 <span style={{fontSize: "inherit"}}>{ i18nextHeader.t('Header.TOPBAR.enter') }</span>
                             </Button>
                         </Link>
@@ -123,7 +170,7 @@ export default function UserBanner({ src, className, ...rest }) {
         return (
             <div sx={styles.userContainer}>
                 <div sx={innerContain}>
-                    <Link
+                    {/* <Link
                         path={process.env.NEXT_PUBLIC_HOME_BASE_URL + "/admin/auth/login/cover"}
                         aria-label={ i18nextHeader.t('Header.TOPBAR.enterLinkInfo') }
                         style={{color: 'inherit', cursor: 'pointer', textDecoration: 'none', padding: 0, display: 'inline-block'}}
@@ -131,18 +178,20 @@ export default function UserBanner({ src, className, ...rest }) {
                         <Button>
                             <span style={{fontSize: "inherit"}}>{ i18nextHeader.t('Header.TOPBAR.enter') }</span>
                         </Button>
-                    </Link>
+                    </Link> */}
                     {/* <div sx={styles.userContainer.userInlineImage}>
                         <Image src={src} alt="User" />
                     </div> */}
+                    {renderLinkContent()}
                 </div>
             </div>
         );
     }
 
     return (
-        className === 'inlineBlock'
-        ? renderInnerBlock()
-        : renderBlock()
+        renderUserOptions()
+        // className === 'inlineBlock'
+        // ? renderInnerBlock()
+        // : renderBlock()
     );
 }
--- frontends/packages/tp_home/components/user/UserBanner.style.js
+++ frontends/packages/tp_home/components/user/UserBanner.style.js
@@ -60,6 +60,69 @@ export const UserBannerStyles = {
             svg: {
                 marginTop: '-3px'
             }
+        },
+        userOptionsContainer: {
+            paddingLeft: '2px',
+            boxSizing: 'border-box',
+            height: '48px',
+            lineHeight: 'normal',
+            padding: '0 4px',
+            flex: '0 0 auto',
+            justifyContent: 'flex-end',
+            verticalAlign: 'middle',
+            whiteSpace: 'nowrap',
+            alignItems: 'center',
+            display: 'flex',
+            appsMenuContainer: {
+                paddingLeft: 0,
+                padding: '4px 2px',
+                display: 'inline-block',
+                verticalAlign: 'middle',
+                appsMenuButton: {
+                    color: '#000',
+                    cursor: 'pointer',
+                    textDecoration: 'none',
+                    backgroundPosition: '-64px -29px',
+                    borderRadius: '50%',
+                    padding: '8px',
+                    display: 'inline-block',
+                    verticalAlign: 'middle',
+                    boxSizing: 'border-box',
+                    height: '40px',
+                    width: '40px',
+                    '&:hover': {
+                        backgroundColor: 'rgba(60,64,67,.08)',
+                        outline: 'none'
+                    },
+                    '&:focus': {
+                        opacity: 1
+                    },
+                    appsMenuIcon: {
+                        color: '#5f6368',
+                        opacity: 1,
+                        fill: 'currentColor'
+                    }
+                },
+                userIdContainer: {
+                    marginLeft: '10px',
+                    marginRight: '4px',
+                    backgroundColor: 'rgba(255,255,255,.88)',
+                    border: '1px solid #dadce0',
+                    boxSizing: 'border-box',
+                    cursor: 'pointer',
+                    display: 'inline-block',
+                    maxHeight: '48px',
+                    overflow: 'hidden',
+                    outline: 'none',
+                    padding: 0,
+                    verticalAlign: 'middle',
+                    width: '134px',
+                    borderRadius: '8px',
+                    lineHeight: 'normal',
+                    whiteSpace: 'nowrap',
+                    font: '13px/27px Roboto,Arial,sans-serif'
+                }
+            }
         }
     },
 }
