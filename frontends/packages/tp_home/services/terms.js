e0bf7c6ce7092582336125c0cda0fb9052c8b0ed
JoaoOxOc <jp_69_7@hotmail.com>
2022-11-19 16:10:47 +0000

    HOME - get terms server side

--- frontends/packages/tp_home/services/terms.js
+++ frontends/packages/tp_home/services/terms.js
@@ -8,11 +8,11 @@ export const GetPublicTerms = async(browserLanguage) => {
         'Accept': '*/*',
         "Content-Type": "application/json",
         "credentials": 'include',
-        "ClientId": process.env.AUTH_API_CLIENT_ID,
-        "ClientAuthorization": process.env.AUTH_API_CLIENT_SECRET
+        "ClientId": process.env.HOME_API_CLIENT_ID,
+        "ClientAuthorization": process.env.HOME_API_CLIENT_SECRET
         };
         // TODO: replace constant lang with the browser 'userBrowserLanguage'?
-        const res = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL + "/terms/public/get" + "?langCode=" + "pt-pt", {
+        const res = await fetch(process.env.HOME_API_BASE_URL + "/terms/public/get" + "?langCode=" + "pt-pt", {
         method: "GET",
         resolveWithFullResponse: true,
         headers: headers,
