a467d24efb2a6c441823302b9303fc38ebc1757c
JoaoOxOc <jp_69_7@hotmail.com>
2023-01-04 18:21:34 +0000

    building timeline page

--- frontends/packages/tp_home/pageSections/timeline/index.js
+++ frontends/packages/tp_home/pageSections/timeline/index.js
@@ -1,16 +1,49 @@
 /** @jsxImportSource theme-ui */
 import { Container } from 'theme-ui';
 import parse from 'html-react-parser';
-import dynamic from 'next/dynamic';
+import { Chrono } from "react-chrono";
 
 import { TimelinePageSectionStyles as styles } from './timeline.style';
 
 export default function TimelinePageSection({timelineCardsContent}) {
     console.log(timelineCardsContent)
 
+    const items = [{
+        title: "May 1940",
+        cardTitle: "Dunkirk",
+        url: "http://www.history.com",
+        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
+        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
+        media: {
+          type: "IMAGE",
+          source: {
+            url: "http://someurl/image.jpg"
+          }
+        }
+      },{
+        title: "May 1940",
+        cardTitle: "Dunkirk",
+        cardSubtitle:
+          "Men of the British Expeditionary Force (BEF) wade out to a destroyer during the evacuation from Dunkirk.",
+        cardDetailedText: ["paragraph1", "paragraph2"],
+      },{
+        title: "7 December 1941",
+        cardTitle: "Pearl Harbor",
+        media: {
+          source: {
+            url: "https://www.youtube.com/embed/f6cz9gtMTeI",
+            type: "mp4"
+          },
+          type: "VIDEO",
+          name: "Pearl Harbor"
+        }
+      }];
+
     return (
         <Container sx={styles.timelinePageSectionContainer}>
-
+            <div style={{ width: '100%', height: '70vh' }}>
+                <Chrono items={items} slideShow mode="VERTICAL_ALTERNATING" scrollable={{ scrollbar: true }} enableOutline/>
+            </div>
         </Container>
     );
 }
