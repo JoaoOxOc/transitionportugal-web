e1cf52fc8eca93e9e6a6c90767b7cdb984cc723b
JoaoOxOc <jp_69_7@hotmail.com>
2023-01-07 15:20:27 +0000

    CONTACTUS - building the section

---
+++ frontends/packages/tp_home/pageSections/contact/contact.js
@@ -0,0 +1,15 @@
+/** @jsxImportSource theme-ui */
+
+import { Container, Flex, Box, Heading, Text, Image, Button } from 'theme-ui';
+
+import { ContactStyles as styles } from './contact.style';
+
+export default function Contact({aboutComponentObject}) {
+    return (
+        <section id="contactUs" sx={styles.contactUs}>
+            <Container>
+
+            </Container>
+        </section>
+    )
+}
---
+++ frontends/packages/tp_home/pageSections/contact/contact.style.js
@@ -0,0 +1,5 @@
+export const ContactStyles = {
+    contactUs: {
+        width: '100%',
+    },
+}
