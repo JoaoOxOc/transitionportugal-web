44b111affdc670e13de6a10de720d8a7f665f5b9
JoaoOxOc <jp_69_7@hotmail.com>
2023-03-04 12:11:51 +0000

    BACKEND - added public send email route

--- tp_webservices/APIs/EmailService/Controllers/EmailController.cs
+++ tp_webservices/APIs/EmailService/Controllers/EmailController.cs
@@ -1,23 +1,23 @@
 ﻿using CommonLibrary.Entities.ViewModel;
+using CommonLibrary.Extensions;
 using EmailService.Code;
-using EmailService.Enums;
-using EmailService.Model;
 using EmailService.Repositories;
 using EmailService.Sender;
 using EmailService.Services.RabbitMQ;
-using MassTransit;
+using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
 
-namespace EmailService.Consumers
+namespace EmailService.Controllers
 {
-    public class EmailQueueConsumer : IEmailQueueConsumer, IConsumer<EmailVM>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
     {
         private readonly IEmailService _emailService;
         private readonly IEmailTemplatesRepository _emailTemplateRepo;
         private readonly ISettingsRepository _settingsRepository;
         private readonly IRabbitMQSender _rabbitSender;
-
-        public EmailQueueConsumer(IEmailService emailService, IEmailTemplatesRepository emailTemplateRepo, ISettingsRepository settingsRepository, IRabbitMQSender rabbitSender)
+        public EmailController(IEmailService emailService, IEmailTemplatesRepository emailTemplateRepo, ISettingsRepository settingsRepository, IRabbitMQSender rabbitSender)
         {
             _emailService = emailService;
             _emailTemplateRepo = emailTemplateRepo;
@@ -25,30 +25,32 @@ namespace EmailService.Consumers
             _rabbitSender = rabbitSender;
         }
 
-        public async Task Consume(ConsumeContext<EmailVM> context)
+        [HttpPost]
+        [Route("send")]
+        public async Task<IActionResult> SendEmail([FromBody] EmailVM model)
         {
-            var mailData = EmailTemplateReplacer.ProcessEmailTemplate(_emailTemplateRepo, context.Message);
-            if (mailData != null)
-            {
-                this.SendEmail(mailData);
-            }
-        }
-
-        public async Task SendEmail(EmailVM mailData)
-        {
+            var mailData = EmailTemplateReplacer.ProcessEmailTemplate(_emailTemplateRepo, model);
             List<string> emailTo = mailData.To != null && mailData.To.Count > 0 ? mailData.To : EmailSettingsManager.getAdminEmails(this._settingsRepository);
             bool sent = _emailService.SendMail(emailTo, mailData.Subject, mailData.Body);
             if (!sent)
             {
                 CommonLibrary.Entities.ViewModel.ExceptionModel exceptionModel = new CommonLibrary.Entities.ViewModel.ExceptionModel();
                 exceptionModel.Message = "Email send error";
-                exceptionModel.StackTrace = "EmailQueueConsumer.Consume()";
+                exceptionModel.StackTrace = "EmailController.SendEmail()";
                 exceptionModel.DateLogging = DateTime.UtcNow;
                 exceptionModel.AdminRole = "Admin";
                 exceptionModel.InnerException = null;
                 exceptionModel.InputDataJson = JsonSerializer.Serialize(mailData);
 
                 bool success = await _rabbitSender.PublishExceptionMessage(exceptionModel);
+                return StatusCode(StatusCodes.Status500InternalServerError, null);
+            }
+            else
+            {
+                return Ok(new
+                {
+                    sent = sent
+                });
             }
         }
     }
