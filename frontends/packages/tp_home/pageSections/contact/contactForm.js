8159a09689a37629717bed207d043ec184d19192
JoaoOxOc <jp_69_7@hotmail.com>
2023-03-21 20:47:46 +0000

    HOME- finished contact us form

--- frontends/packages/tp_home/pageSections/contact/contactForm.js
+++ frontends/packages/tp_home/pageSections/contact/contactForm.js
@@ -1,3 +1,222 @@
+import React, { useState } from 'react';
+import * as Yup from 'yup';
+import { Field, useFormik, Formik, ErrorMessage } from 'formik';
+import {
+    Box,
+    Button,
+    FormHelperText,
+    TextField,
+    Grid,
+    Checkbox,
+    Typography,
+    FormControlLabel,
+    CircularProgress
+  } from '@material-ui/core';
+  import {Alert} from '@material-ui/lab';
+  import { i18nextContacts } from "@transitionpt/translations";
+import { ContactStyles as styles } from './contact.style';
 
-export default function ContactForm({aboutComponentObject}) {
+export default function ContactForm({contactComponentObject}) {
+    const { t } = i18nextContacts;
+    const [messageSent, setMessageSent] = useState(false);
+    const [messageSentError, setMessageSentError] = useState('');
+
+    const messageSendError = ({ error }) => {
+      return (
+        <>
+          <Alert
+            sx={{
+              mb: 1
+            }}
+            severity="error"
+            aria-label={ t('MESSAGES.messageSentErrorResult') }
+          >
+            <span>{t('MESSAGES.messageSentError')}</span>
+          </Alert>
+        </>
+      );
+    };
+
+    const messageSentLabel = () => {
+      return (
+        <>
+          <Alert
+            sx={{
+              mb: 1
+            }}
+            severity="success"
+            aria-label={ t('MESSAGES.messageSentResult') }
+          >
+            <span>{t('MESSAGES.messageSent')}</span>
+          </Alert>
+        </>
+      );
+    };
+
+    const formik = useFormik({
+        initialValues: {
+          fromPhone: '',
+          fromEmail: '',
+          fromName: '',
+          messageSubject: '',
+          messageBody: '',
+          submit: null
+        },
+        validationSchema: Yup.object({
+          fromEmail: Yup.string()
+            .email(t('MESSAGES.fromEmailInvalid'))
+            .max(255)
+            .required(t('MESSAGES.fromEmailRequired')),
+          fromName: Yup.string()
+            .max(255)
+            .required(t('MESSAGES.fromNameRequired')),
+          messageSubject: Yup.string()
+            .max(255)
+            .required(t('MESSAGES.messageSubjectRequired')),
+          messageBody: Yup.string()
+            .max(5000)
+            .required(t('MESSAGES.messageBodyRequired'))
+        }),
+        onSubmit: async (values, helpers) => {
+          if (values.fromPhone.length != 0) {
+            return null;
+          }
+          //helpers.validateForm(values).then((e) => {helpers.setSubmitting(true); contactUsFormElement.current.submit(values);})
+          // console.log(helpers);
+          helpers.setSubmitting(true);
+          const emailSent = await fetch(process.env.NEXT_PUBLIC_HOME_BASE_URL + "/api/contactUsAction", {
+            method: 'POST',
+            body: JSON.stringify({ messageData: values }),
+            headers: { 
+              "Content-Type": "application/json",
+              "credentials": 'include'
+            }
+          });
+          const emailSentParsed = await emailSent.json();
+          console.log(emailSentParsed, emailSent);
+          if (emailSent.status != 200) {
+            setMessageSentError('errorMessage');
+            helpers.setStatus({ success: false });
+            helpers.setErrors({ submit: emailSent.error });
+            helpers.setSubmitting(false);
+          }
+          else {
+            helpers.setStatus({ success: true });
+            helpers.setErrors({ submit: null });
+            formik.resetForm();
+            setMessageSent(true);
+          }
+        }
+      });
+
+    return (
+      <>
+      {messageSentError && messageSentError != '' && 
+        messageSendError(messageSentError)
+      }
+      {messageSent && 
+        messageSentLabel()
+      }
+        <form key="contactUsFormElement" onSubmit={formik.handleSubmit} method="POST">
+          
+          <TextField
+              style={{
+                display: 'none'
+              }}
+              InputProps={{
+                style: {display: 'none'}
+              }}
+              fullWidth
+              name="fromPhone"
+              label={t('FORMS.fromPhone')}
+              aria-labelledby={ t('FORMS.fromPhone') } 
+              aria-describedby={ t('FORMS.fromPhone_help') }
+            />
+          <TextField
+            error={Boolean(formik.touched.fromEmail && formik.errors.fromEmail)}
+            fullWidth
+            margin="normal"
+            autoFocus
+            helperText={formik.touched.fromEmail && formik.errors.fromEmail}
+            label={t('FORMS.emailFrom')}
+            aria-labelledby={ t('FORMS.emailFrom') }
+            aria-describedby={ t('FORMS.emailFrom_help') }
+            name="fromEmail"
+            placeholder='email'
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            type="email"
+            value={formik.values.fromEmail}
+          />
+          <TextField
+            error={Boolean(formik.touched.fromName && formik.errors.fromName)}
+            fullWidth
+            margin="normal"
+            helperText={formik.touched.fromName && formik.errors.fromName}
+            label={t('FORMS.fromName')}
+            aria-labelledby={ t('FORMS.fromName') }
+            aria-describedby={ t('FORMS.fromName_help') }
+            name="fromName"
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            type="text"
+            value={formik.values.fromName}
+          />
+          <TextField
+            error={Boolean(formik.touched.messageSubject && formik.errors.messageSubject)}
+            fullWidth
+            margin="normal"
+            helperText={formik.touched.messageSubject && formik.errors.messageSubject}
+            label={t('FORMS.messageSubject')}
+            aria-labelledby={ t('FORMS.messageSubject') }
+            aria-describedby={ t('FORMS.messageSubject_help') }
+            name="messageSubject"
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            type="text"
+            value={formik.values.messageSubject}
+          />
+          <TextField
+            error={Boolean(formik.touched.messageBody && formik.errors.messageBody)}
+            fullWidth
+            multiline
+            minRows={4}
+            maxRows={8}
+            margin="normal"
+            helperText={formik.touched.messageBody && formik.errors.messageBody}
+            label={t('FORMS.messageBody')}
+            aria-labelledby={ t('FORMS.messageBody') }
+            aria-describedby={ t('FORMS.messageBody_help') }
+            name="messageBody"
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            type="text"
+            value={formik.values.messageBody}
+            variant="outlined"
+          />
+
+          {Boolean(formik.touched.terms && formik.errors.terms) && (
+            <FormHelperText error>{formik.errors.terms}</FormHelperText>
+          )}
+
+          <Button
+            sx={{
+              mt: 3
+            }}
+            color="primary"
+            startIcon={
+              formik.isSubmitting ? <CircularProgress size="1rem" /> : null
+            }
+            disabled={formik.isSubmitting}
+            aria-describedby={ t('FORMS.submit_help') }
+            type="submit"
+            fullWidth
+            size="large"
+            variant="contained"
+          >
+            {t('FORMS.sendButton')}
+          </Button>
+        </form>
+      </>
+    )
 }
--- frontends/packages/tp_translations/src/consts/Translations/pt/home/Components/contacts.ts
+++ frontends/packages/tp_translations/src/consts/Translations/pt/home/Components/contacts.ts
@@ -3,8 +3,35 @@ import * as deepFreeze from 'deep-freeze';
 export const CONTACTS_TRANSLATIONS = deepFreeze({
     CONTACTS: {
         email_input_label: "Email",
-        email_input_help: "Insert the email where you will receive the newsletter",
-        email_input_placeholder: "Your email...",
-        subscribe_button: "Subscribe"
+        email_input_help: "Insira o email onde irá receber a newsletter",
+        email_input_placeholder: "O seu email...",
+        subscribe_button: "Subscrever"
     },
+    LABELS: {
+        contactUsTitle: "Contacte-nos",
+        contactUsMessage: "Tem dúvidas sobre o movimento transição? Por favor envie-nos uma mensagem e tentaremos esclarecer as suas dúvidas"
+    },
+    FORMS: {
+        emailFrom: "O seu Email",
+        emailFrom_help: "Insira o email que iremos usar para o contactar",
+        fromName: "O seu Nome",
+        fromName_help: "Insira o nome que o identificará na nossa tentativa de o contactar",
+        messageSubject: "Assunto",
+        messageSubject_help: "Insira o assunto em causa para esta mensagem",
+        messageBody: "Mensagem",
+        messageBody_help: "Insira a mensagem que nos deseja transmitir",
+        sendButton: "Enviar",
+        submit_help: "Este botão irá enviar a mensagem de contacto com os dados inseridos"
+    },
+    MESSAGES: {
+        fromEmailInvalid: "O email que inseriu não é válido",
+        fromEmailRequired: "Por favor indique-nos o seu email para o contactar",
+        fromNameRequired: "Por favor indique-nos o seu nome",
+        messageSubjectRequired: "Por favor indique-nos o seu assunto",
+        messageBodyRequired: "Por favor escreva-nos uma mensagem",
+        messageSent: "Grato, mensagem enviada. Tentaremos responder o mais brevemente possível.",
+        messageSentResult: "Apresenta a mensagem de sucesso no envio da mensagem",
+        messageSentError: "Ocorreu um erro ao enviar a mensagem. Por favor tente novamente. Se o problema persistir, por favor entre em contacto connosco por outros meios.",
+        messageSentErrorResult: "Apresenta a mensagem de erro, caso falhe o envio da mensagem"
+    }
 } as const);
